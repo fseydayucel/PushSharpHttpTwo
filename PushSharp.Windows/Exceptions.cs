048d7c467e23fffdc97d4607b6c5cff84a8a854e
Şeyda Demetoğlu <fseydayucel@gmail.com>
2020-02-06 14:24:36 +0300

    Initial Commit

---
+++ PushSharp.Windows/Exceptions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using PushSharp.Core;
+
+namespace PushSharp.Windows
+{
+    public class WnsNotificationException : NotificationException
+    {
+        public WnsNotificationException (WnsNotificationStatus status) : base (status.ErrorDescription, status.Notification) 
+        {
+            Notification = status.Notification;
+            Status = status;
+        }
+
+        public new WnsNotification Notification { get; set; }
+        public WnsNotificationStatus Status { get; private set; }
+
+        public override string ToString ()
+        {
+            return base.ToString() + " Status = " + Status.HttpStatus;
+        }
+    }
+}
+
